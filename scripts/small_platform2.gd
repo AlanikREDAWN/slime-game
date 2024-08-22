47315f23468f63199252fca81977e942bdc49fa8
Alanik <lilia.dostie@socprep.org>
2024-08-22 13:28:47 -0400

    more progress!

--- scripts/small_platform2.gd
+++ scripts/small_platform2.gd
@@ -3,9 +3,8 @@ extends AnimatableBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	await get_tree().create_timer(1.0).timeout
+	await get_tree().create_timer(2.0).timeout
 	$AnimationPlayer.play("move")
-
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
