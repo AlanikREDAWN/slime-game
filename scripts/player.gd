192770fccf3173653ac61814c8d7ea0519175909
Alanik <lilia.dostie@socprep.org>
2024-08-23 13:29:12 -0400

    Revert "Progress"
    This reverts commit c1c05fe1b520e0880c719f304bfe6fdea08a6119.

--- scripts/player.gd
+++ scripts/player.gd
@@ -28,9 +28,7 @@ func _physics_process(delta: float) -> void:
 	
 	# Play animations
 	if is_on_floor():
-		if Input.is_action_pressed("duck"):
-			animated_sprite.play("duck")
-		elif direction == 0:
+		if direction == 0:
 			animated_sprite.play("idle")
 		else:
 			animated_sprite.play("run")
