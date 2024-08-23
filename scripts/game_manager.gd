aafb12c9b6489c2aa30177c65367a78bc3593cc2
Alanik <lilia.dostie@socprep.org>
2024-08-23 13:00:36 -0400

    Revert "Progress"
    This reverts commit c1c05fe1b520e0880c719f304bfe6fdea08a6119.

--- scripts/game_manager.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-@onready var coins_label: Label = $CoinsLabel
-
-var coinsCollected = 0
-
-func add_coin():
-	coinsCollected += 1
-	coins_label.text = "Coins: " + str(coinsCollected)
