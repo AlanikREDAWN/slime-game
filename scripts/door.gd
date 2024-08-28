43102aa9de5df0b71f81ccd8859aca61e3a3aa76
Alanik <lilia.dostie@socprep.org>
2024-08-28 17:05:54 -0400

    commit

--- scripts/door.gd
+++ scripts/door.gd
@@ -5,8 +5,14 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	print(self.name)
 
+func _physics_process(delta: float) -> void:
+	if Global.on_door == true:
+		if Input.is_action_pressed("down"):
+			Global.doors_entered.append(self.name)
+			print(Global.doors_entered)
+			player.position = Vector2(198, 1299)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -15,10 +21,7 @@ func _process(delta: float) -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	Global.on_door = true
-	if Input.is_action_pressed("down"):
-		Global.doors_entered.append(self.name)
-		print(Global.doors_entered)
-		player.position = Vector2(198, 1299)
+	
 
 
 func _on_body_exited(body: Node2D) -> void:
